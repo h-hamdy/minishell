49468a41d670c4eced948de8c581494f45077463
Houssam Hamdy <hhamdy@e2r2p3.1337.ma>
2022-05-22 10:16:01 +0100

    done

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: hhamdy <hhamdy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 09:23:59 by hhamdy            #+#    #+#             */
-/*   Updated: 2022/05/21 12:59:58 by hhamdy           ###   ########.fr       */
+/*   Updated: 2022/05/22 10:11:18 by hhamdy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,33 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <limits.h>
+# include <curses.h>
+# include <term.h>
+# include <signal.h>
+# include <sys/stat.h>
 
 # include "libft/libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef	struct	s_node
+{
+	char		*cmd;
+	char		*arguments;
+	char		*infile;
+	char		*outfile;
+	char		*limiter;
+}				t_node;
+
+typedef struct	s_data
+{
+	t_node	*node;
+	char	**path;
+	pid_t	pid;
+}				t_data;
+
 /******************PARCING_PART*****************/
-
 void	parcing_function(void);
-
 /*-----------------------------------------------*/
 
 #endif
--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,15 +6,47 @@
 /*   By: hhamdy <hhamdy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 11:06:20 by hhamdy            #+#    #+#             */
-/*   Updated: 2022/05/21 12:02:50 by hhamdy           ###   ########.fr       */
+/*   Updated: 2022/05/22 10:11:36 by hhamdy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	error_msg(char *s)
+{
+	printf("%s", s);
+}
+
+void	double_quotes(char *line)
+{
+	int i;
+	int flag;
+
+	i = 0;
+	flag = 0;
+	while (line[i])
+	{
+		if (line[i] == '"')
+			flag++;
+		i++;
+	}
+	if (flag % 2 != 0)
+		error_msg("double quotes error\n");
+}
+
+void	split_line(char *line)
+{
+	double_quotes(line);
+}
+
 void	parcing_function(void)
 {
-	char	*inpt;
+	char	*line;
 
-	inpt = readline("minishell: ");
+	while (1)
+	{
+		line = readline("\033[0;36mMinishell$ \033[0;37m");
+        add_history(line);
+		split_line(line);
+	}
 }
